a7a231fa9825f8f0a55c5fa0509d391ae40aa099
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-11-08 04:50:46 +0800

    MongoDB config added

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.get("/", (req, res) => {
   res.send("Mom's Kitchen Server is running");
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@userc1.twqeubr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
 });
